76700b223f360299bcb29e5d5baab1f41c244894
afmcmorais <afmcmorais@icloud.com>
2024-08-28 11:49:38 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+// Criando a aplicação Express
+const app = express();
+
+// Conectando ao MongoDB
+mongoose.connect(process.env.DATABASE_URL)
+    .then(() => {
+        console.log('Conectado ao MongoDB');
+    }).catch((err) => {
+        console.error('Erro ao conectar ao MongoDB:', err.message);
+    });
+
+// Middleware para permitir JSON nas requisições
+app.use(express.json());
+
+// Usando as rotas definidas em routes.js
+app.use('/api', routes);
+
+// Iniciando o servidor
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
